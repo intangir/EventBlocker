c272e82c2540b296f05d3ac3862b2dfbb02d308b
Intangir <intangir@gmail.com>
2012-12-30 22:07:59 -0600

    blocks most of the events i wanted

--- src/com/github/intangir/EventBlocker/EventBlocker.java
+++ src/com/github/intangir/EventBlocker/EventBlocker.java
@@ -1,49 +1,109 @@
-package com.github.intangir.RingTransporters;
+package com.github.intangir.EventBlocker;
 
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Biome;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.ChunkPopulateEvent;
+import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class EventBlocker extends JavaPlugin implements Listener {
-
+public class EventBlocker extends JavaPlugin implements Listener
+{
     public Logger log;
     public PluginDescriptionFile pdfFile;
-    public PlateListener plateListener;
     
-	public void onEnable() {
+    public void info(String message)
+    {
+    	log.info("[" + pdfFile.getName() + "] " + message);
+    }
+    
+	public void onEnable()
+	{
 		log = this.getLogger();
 		pdfFile = this.getDescription();
-		plateListener = new PlateListener();
 
-		Bukkit.getPluginManager().registerEvents(plateListener, this);
-
-
+		Bukkit.getPluginManager().registerEvents(this, this);
 		
-		log.info(pdfFile.getName() + " v" + pdfFile.getVersion() + " enabled!");
+		info("v" + pdfFile.getVersion() + " enabled!");
 	}
 	
-	public void onDisable() {
-		log.info(pdfFile.getName() + " v" + pdfFile.getVersion() + " disabled.");
+	public void onDisable()
+	{
+		info("v" + pdfFile.getVersion() + " disabled.");
 	}
 
+	// block villager trading
 	@EventHandler(ignoreCancelled=true)
-	public void onEntityInteract(EntityInteractEvent e) {
-		if(e.getBlock().getType() == Material.STONE_PLATE) {
-			e.getEntity().getWorld().playEffect(e.getBlock().getLocation(), Effect.MOBSPAWNER_FLAMES, 0);
-			e.getEntity().getWorld().playEffect(e.getBlock().getLocation(), Effect.ENDER_SIGNAL, 0);
-		}
+	public void onEntityInteract(PlayerInteractEntityEvent e)
+	{
+	    Entity npc = e.getRightClicked();
+	    if (npc == null)
+	    {
+	        return;
+	    }
+	    
+	    if (npc.getType() == EntityType.VILLAGER)
+	    {
+	      e.setCancelled(true);
+	    }
     }
 
+	// block enderchests 
 	@EventHandler(ignoreCancelled=true)
-	public void onPlayerInteract(PlayerInteractEvent e) {
-		if(e.getClickedBlock().getType() == Material.STONE_PLATE) {
-			e.getPlayer().getWorld().playEffect(e.getClickedBlock().getLocation(), Effect.MOBSPAWNER_FLAMES, 0);
-			e.getPlayer().getWorld().playEffect(e.getClickedBlock().getLocation(), Effect.ENDER_SIGNAL, 0);
+	public void onPlayerInteract(PlayerInteractEvent e)
+	{
+		if(e.getClickedBlock().getType() == Material.ENDER_CHEST)
+		{
+			e.setCancelled(true);
 		}
-
+	}
+	@EventHandler(ignoreCancelled=true)
+	public void onBlockPlace(BlockPlaceEvent e)
+	{
+		if(e.getBlock().getType() == Material.ENDER_CHEST)
+		{
+			e.setCancelled(true);
+		}
 	}
 	
+	// block portals
+	@EventHandler(ignoreCancelled=true)
+	public void onPortalCreate(PortalCreateEvent e)
+	{
+		e.setCancelled(true);
+	}
+
+	// temporary spawner switcher
+	@EventHandler(ignoreCancelled=true)
+	public void onChunkPopulate(ChunkPopulateEvent e)
+	{
+		for (BlockState state: e.getChunk().getTileEntities())
+		{
+			if(state instanceof CreatureSpawner)
+			{
+				if(state.getBlock().getBiome() == Biome.HELL)
+				{
+					EntityType et = ((CreatureSpawner) state).getSpawnedType();
+					if(et == EntityType.ZOMBIE || et == EntityType.SKELETON)
+					{
+						((CreatureSpawner) state).setSpawnedType(EntityType.BLAZE);
+						info("Changed spawner to blaze");
+					}
+				}
+			}
+		}
+	}
 }
 
